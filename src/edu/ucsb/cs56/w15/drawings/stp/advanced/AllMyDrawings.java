fc90ac91c00299efcdc1d82ab2b5da16fcb296bd
Spencer Prescott <stp@umail.ucsb.edu>
2015-02-08 15:17:19 -0800

    Add scooter and scooter with basket

--- src/edu/ucsb/cs56/w15/drawings/stp/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/stp/advanced/AllMyDrawings.java
@@ -20,140 +20,129 @@ import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
  * A class with static methods for drawing various pictures
  *
  * @author Phill Conrad
- * @version for CS10, lab06, Spring 2009
+ * @author Spencer Prescott
+ * @version for CS56, lab04, Winter 2015
  */
 
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses
+    /** Draw a picture with a few Scooters
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+    	Scooter s1 = new Scooter(10,200,50,70, 20);
+    	g2.setColor(Color.CYAN);
+      g2.draw(s1);
 
-	// Make a black house that's half the size,
-	// and moved over 150 pixels in x direction
+    	//Make a black Scooter that's half the size,
+    	//and moved over 150 pixels in x direction
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+    	Shape s2 = ShapeTransforms.scaledCopyOfLL(s1,0.5,0.5);
+    	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
+    	g2.setColor(Color.BLACK);
+      g2.draw(s2);
 
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+    	// Here's a Scooter that's 4x as big (2x the original)
+    	// and moved over 100px to the right and 20px down.
+    	s2 = ShapeTransforms.scaledCopyOfLL(s2,4,4);
+    	s2 = ShapeTransforms.translatedCopyOf(s2,100,20);
 
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+    	// We'll draw this with a thicker stroke
+    	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
 
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7));
-	g2.draw(h2);
+    	Stroke orig=g2.getStroke();
+    	g2.setStroke(thick);
+    	g2.setColor(new Color(0x9999FF));
+    	g2.draw(s2);
 
-	// Draw two houses with Windows
 
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
 
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+    	ScooterWithBasket sb1 = new ScooterWithBasket(50,350,40,50, 10, 10, 10);
+      ScooterWithBasket sb2 = new ScooterWithBasket(200,350,200,300, 100, 40, 40);
 
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
+    	g2.draw(sb1);
+    	g2.setColor(new Color(0x8F00FF)); g2.draw(sb2);
 
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK);
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+
+    	g2.setStroke(orig);
+    	g2.setColor(Color.BLACK);
+    	g2.drawString("Some scooters by Spencer Prescott", 20,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a Scooter variations
      */
     public static void drawPicture2(Graphics2D g2) {
 
-	// Draw some coffee cups.
+    	// Draw some scooters.
 
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+    	Scooter large = new Scooter(200,180,130,150,60);
+      Scooter small = new Scooter(20,230,40,50,10);
 
-	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
+    	g2.setColor(Color.RED);     g2.draw(large);
+    	g2.setColor(Color.GREEN);   g2.draw(small);
 
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+    	ScooterWithBasket sb1 = new ScooterWithBasket(100,250,50,75,20,20,20);
+    	g2.setColor(Color.PINK); g2.draw(sb1);
 
-	// Make a black house that's half the size,
-	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+    	Shape sb2 = ShapeTransforms.scaledCopyOfLL(sb1,0.5,0.5);
+    	sb2 = ShapeTransforms.translatedCopyOf(sb2,150,0);
+    	g2.setColor(Color.BLACK);
+      g2.draw(sb2);
 
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+    	// Here's a Scooter with a basket that's 4x as big (2x the original)
+    	// and moved over 150 more pixels to right.
+    	sb2 = ShapeTransforms.scaledCopyOfLL(sb2,4,4);
+    	sb2 = ShapeTransforms.translatedCopyOf(sb2,150,0);
 
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+    	// We'll draw this with a thicker stroke
+    	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
 
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7));
-	g2.draw(h2);
+    	Stroke orig=g2.getStroke();
+    	g2.setStroke(thick);
+    	g2.setColor(new Color(0x00FF00));
+    	g2.draw(sb2);
 
-	// Draw two houses with Windows
+    	// Draw two Scooters with baskets
 
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+    	ScooterWithBasket sb3 = new ScooterWithBasket(50,350,40,75,10,10,10);
+      ScooterWithBasket sb4 = new ScooterWithBasket(200,350,100,125,40,20,20);
 
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF));
+    	g2.draw(sb3);
+    	g2.setColor(new Color(0xFF9900));
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+    	// Rotate the second Scooter 45 degrees around its center.
+    	Shape sb5 = ShapeTransforms.rotatedCopyOf(sb4, Math.PI/4.0);
 
-	g2.draw(hw3);
+    	g2.draw(sb5);
 
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK);
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+    	g2.setStroke(orig);
+    	g2.setColor(Color.BLACK);
+    	g2.drawString("A lot of scooter variations drawn by Spencer Prescott", 20,20);
     }
 
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a couple Scooters
      */
 
     public static void drawPicture3(Graphics2D g2) {
 
-	// label the drawing
+      // label the drawing
 
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+      g2.drawString("A bunch of Scooters by Spencer Prescott", 20,20);
 
 
-	// Draw some coffee cups.
+      // Draw some scooters.
 
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+      Scooter large = new Scooter(100,50,225,275,130);
+      Scooter small = new Scooter(20,50,20,40,5);
 
-       g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
-
+      g2.setColor(Color.PINK);     g2.draw(large);
+      g2.setColor(Color.ORANGE);   g2.draw(small);
 
     }
 
